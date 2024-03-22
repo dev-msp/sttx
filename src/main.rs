3cff9290c8cc5f780478db9036d800de369e1511
Matt Palmer <palmer.s.matt@gmail.com>
2024-03-22 16:19:20 -0400

    Abstract the bad CSV formatting as a vendor concern

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,7 @@
 mod transcribe;
 mod vendor;
 
-use std::{
-    io::{BufReader, BufWriter},
-    time::Duration,
-};
+use std::time::Duration;
 
 use crate::transcribe::TxbIter;
 
@@ -14,11 +11,8 @@ fn main() {
     let args: Vec<String> = std::env::args().collect();
     let file_path = &args[1];
     let file = std::fs::File::open(file_path).unwrap();
-    let r = BufReader::new(file);
-    let mut bytes = Vec::new();
-    vendor::handle_quotes(r, BufWriter::new(&mut bytes)).unwrap();
 
-    let mut rdr = csv::Reader::from_reader(bytes.as_slice());
+    let mut rdr = csv::Reader::from_reader(vendor::BadCsvReader::new(file));
     let timings = rdr
         .deserialize()
         .filter_map(|r: TxResult| r.ok())
