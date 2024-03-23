d38e6928ef3dda8d9d2a0176573658d5455f4201
Matt Palmer <palmer.s.matt@gmail.com>
2024-03-23 17:47:02 -0400

    Make it command-line-driven
    - Add dependency: clap

---
+++ src/args.rs
@@ -0,0 +1,175 @@
+use std::time::Duration;
+
+use clap::{
+    error::{ContextKind, ContextValue, ErrorKind},
+    Error, Parser,
+};
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+pub struct App {
+    #[arg(long, value_parser = ParseDuration)]
+    max_silence: Option<Duration>,
+
+    #[arg(short, long, default_value = "false")]
+    sentences: bool,
+
+    #[arg(short = 'w', long)]
+    min_word_count: Option<usize>,
+
+    #[arg(short = 'g', long, value_parser = ParseDuration)]
+    by_gap: Option<Duration>,
+
+    #[arg(short, long, value_parser = ParseDuration)]
+    lasting: Option<Duration>,
+
+    #[arg(short, long, default_value = "pretty", value_parser = OutputKind::parse)]
+    output: OutputKind,
+
+    path: String,
+
+    #[arg(default_value = "-", value_parser = OutputSink::parse)]
+    output_sink: OutputSink,
+}
+
+#[derive(Debug, Clone)]
+pub enum OutputKind {
+    Csv,
+    Pretty,
+}
+
+impl OutputKind {
+    fn parse(s: &str) -> Result<Self, String> {
+        match s {
+            "csv" => Ok(Self::Csv),
+            "pretty" => Ok(Self::Pretty),
+            _ => Err("invalid output kind".to_string()),
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub enum OutputSink {
+    Stdout,
+    File(String),
+}
+
+impl OutputSink {
+    fn parse(s: &str) -> Result<Self, String> {
+        if s == "-" {
+            Ok(Self::Stdout)
+        } else {
+            Ok(Self::File(s.to_string()))
+        }
+    }
+}
+
+#[allow(dead_code)]
+impl App {
+    pub fn path(&self) -> &str {
+        &self.path
+    }
+
+    pub fn max_silence(&self) -> Option<Duration> {
+        self.max_silence
+    }
+
+    pub fn min_word_count(&self) -> Option<usize> {
+        self.min_word_count
+    }
+
+    pub fn by_gap(&self) -> Option<Duration> {
+        self.by_gap
+    }
+
+    pub fn lasting(&self) -> Option<Duration> {
+        self.lasting
+    }
+
+    pub fn sentences(&self) -> bool {
+        self.sentences
+    }
+
+    pub fn output(&self) -> OutputKind {
+        self.output.clone()
+    }
+
+    pub fn sink(&self) -> std::io::Result<Box<dyn std::io::Write>> {
+        let writer: Box<dyn std::io::Write> = match self.output_sink {
+            OutputSink::Stdout => Box::new(std::io::stdout()),
+            OutputSink::File(ref path) => Box::new(std::fs::File::create(path)?),
+        };
+        Ok(writer)
+    }
+}
+
+#[derive(Debug, Clone)]
+struct ParseDuration;
+
+impl clap::builder::TypedValueParser for ParseDuration {
+    type Value = Duration;
+
+    fn parse_ref(
+        &self,
+        _cmd: &clap::Command,
+        arg: Option<&clap::Arg>,
+        value: &std::ffi::OsStr,
+    ) -> Result<Self::Value, clap::Error> {
+        let error = |kind: ErrorKind, msg: &str| -> clap::Error {
+            let attribution = arg.map(|arg| format!(" for option '{}'", arg.get_id()));
+            let mut e = Error::new(kind);
+            e.insert(
+                ContextKind::Custom,
+                ContextValue::String(
+                    match attribution {
+                        Some(attribution) => format!("{}{}", msg, attribution),
+                        None => msg.to_string(),
+                    }
+                    .to_owned(),
+                ),
+            );
+            e
+        };
+
+        let Some(s) = value.to_str() else {
+            return Err(error(
+                ErrorKind::MissingRequiredArgument,
+                "didn't receive a string",
+            ));
+        };
+
+        let digits = s
+            .chars()
+            .take_while(|c| c.is_ascii_digit())
+            .collect::<String>();
+
+        if digits.is_empty() {
+            return Err(error(
+                ErrorKind::ValueValidation,
+                "no digits found in value",
+            ));
+        }
+
+        let rest = s.chars().skip(digits.len()).collect::<String>();
+        if rest.is_empty() {
+            return Err(error(ErrorKind::ValueValidation, "no unit found in value"));
+        }
+
+        let Ok(num) = digits.parse::<usize>() else {
+            return Err(error(ErrorKind::ValueValidation, "couldn't parse digits"));
+        };
+
+        let duration = match rest.as_str() {
+            "s" => Duration::from_secs(num as u64),
+            "ms" => Duration::from_millis(num as u64),
+            _ => {
+                return Err(error(
+                    ErrorKind::ValueValidation,
+                    "invalid duration unit; expected 's' or 'ms'",
+                ))
+            }
+        };
+
+        Ok(duration)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,58 @@
+mod args;
 mod transcribe;
 mod vendor;
 
-use std::time::Duration;
+use args::App;
+use clap::Parser;
 
 use crate::transcribe::IteratorExt;
 
 type TxResult = Result<transcribe::Timing, csv::Error>;
 
+impl App {
+    fn read_file(&self) -> std::fs::File {
+        std::fs::File::open(self.path()).unwrap()
+    }
+}
+
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
-    let file_path = &args[1];
-    let file = std::fs::File::open(file_path).unwrap();
+    let app = App::parse();
+
+    let file = app.read_file();
 
     let mut rdr = csv::Reader::from_reader(vendor::BadCsvReader::new(file));
-    let timings = rdr
+    let mut timings = rdr
         .deserialize()
         .map(|r: TxResult| r.expect("no malformed CSV records"))
-        .join_continuations()
-        .sentences()
-        .lasting(Duration::from_secs(10));
+        .join_continuations();
 
-    for t in timings {
-        println!("{}", t);
+    if let Some(silence) = app.max_silence() {
+        timings = timings.max_silence(silence)
+    }
+
+    if let Some(gap) = app.by_gap() {
+        timings = timings.by_gap(gap);
+    }
+
+    if app.sentences() {
+        timings = timings.sentences();
+    }
+
+    if let Some(min_word_count) = app.min_word_count() {
+        timings = timings.min_word_count(min_word_count);
+    }
+
+    if let Some(window) = app.lasting() {
+        timings = timings.lasting(window);
+    }
+
+    let mut s = app.sink().unwrap();
+    match app.output() {
+        args::OutputKind::Csv => timings.write_csv(s).unwrap(),
+        args::OutputKind::Pretty => {
+            for t in timings {
+                writeln!(s, "{}\n", t).unwrap();
+            }
+        }
     }
 }
